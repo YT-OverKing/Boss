31b9b92a83e50787c1bbd3d22b91a2e858633a9e
YT-OverKing <60851021+YT-OverKing@users.noreply.github.com>
2021-02-17 00:55:02 +0700

    Update Working.lua

--- Working.lua
+++ Working.lua
@@ -8,9 +8,13 @@ if game.PlaceId == 6382464838 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/Dev-Hub-V1/main/WhatIsThis.lua"))()    
 else
 if game.PlaceID == 5810056693 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/Reaper.lua"))()        
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/Reaper.lua"))()
+else
+if game.PlaceID == 3823781113 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/Saber.lua"))()            
 else
 print("Game Not Supported")
+end
 end
 end
 end
