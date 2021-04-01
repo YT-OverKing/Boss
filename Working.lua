5262f428c825a9934f97b9d643a03184bb1ad70a
YT-OverKing <60851021+YT-OverKing@users.noreply.github.com>
2021-04-01 16:23:12 +0700

    Update Working.lua

--- Working.lua
+++ Working.lua
@@ -1,21 +1,13 @@
-if game.PlaceId == 6292475469 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/Dev-Hub-V1/main/FPGAgki45G3.lua"))()
+if game.PlaceId == 5835263912 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/ngr.lua"))()
 else
-if game.PlaceId == 2753915549 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/Dev-Hub-V1/main/Gt2FSh6aXg.lua"))()    
+if game.PlaceId == 5303541547 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/ttttttttt"))()    
 else
-if game.PlaceId == 6382464838 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/Dev-Hub-V1/main/WhatIsThis.lua"))()    
-else
-if game.PlaceID == 6378344724 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/1M.lua"))()
-else
-if game.PlaceID == 3823781113 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/DevHub-Paid/master/Saber.lua"))()            
+if game.PlaceId == 6326691606 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YT-OverKing/Dev-Hub-V1/main/three%20piece%20obf.lua"))()           
 else
 print("Game Not Supported")
-end
-end
 end
 end
 end
